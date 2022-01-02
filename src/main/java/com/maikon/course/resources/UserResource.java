6d98c488d71c86c3067528acd56bd1918d047704
Maikon Farias <fariasmk@hotmail.com>
2022-01-01 22:21:24 -0300

    User entity and resource

---
+++ src/main/java/com/maikon/course/resources/UserResource.java
@@ -0,0 +1,20 @@
+package com.maikon.course.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.maikon.course.entities.User;
+
+@RestController //Informa que esta classe é um recurso web implementada por controlador Rest.
+@RequestMapping(value = "/users")
+public class UserResource {
+	
+	@GetMapping
+	public ResponseEntity<User> findAll(){
+		User u = new User(1L, "Maria", "maria@gmail.com", "9999999", "12345");
+		return ResponseEntity.ok().body(u);
+	}
+
+}
