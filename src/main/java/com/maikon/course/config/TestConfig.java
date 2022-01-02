66a9c4940517aedc1fe51ac67543ba281308213f
Maikon Farias <fariasmk@hotmail.com>
2022-01-02 17:25:54 -0300

    Category

--- src/main/java/com/maikon/course/config/TestConfig.java
+++ src/main/java/com/maikon/course/config/TestConfig.java
@@ -8,9 +8,11 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import com.maikon.course.entities.Category;
 import com.maikon.course.entities.Order;
 import com.maikon.course.entities.User;
 import com.maikon.course.entities.enums.OrderStatus;
+import com.maikon.course.repositories.CategoryRepository;
 import com.maikon.course.repositories.OrderRepository;
 import com.maikon.course.repositories.UserRepository;
 
@@ -24,8 +26,18 @@ public class TestConfig implements CommandLineRunner{
 	@Autowired
 	private OrderRepository orderRepository;
 	
+	@Autowired
+	private CategoryRepository categoryRepository;
+	
 	@Override
 	public void run(String... args) throws Exception {
+		
+		Category cat1 = new Category(null, "Electronics");
+		Category cat2 = new Category(null, "Books");
+		Category cat3 = new Category(null, "Computers");
+		
+		categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
+		
 		User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456"); //Ctrl + Shif  + o -->importa User
 		User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
 		
