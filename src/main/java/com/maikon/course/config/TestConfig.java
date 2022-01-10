c6579f6ec1215ce9b180d284d94749c449da7ba7
fariasmk <fariasmk@hotmail.com>
2022-01-09 21:57:58 -0300

    Payment, one-to-one association

--- src/main/java/com/maikon/course/config/TestConfig.java
+++ src/main/java/com/maikon/course/config/TestConfig.java
@@ -12,6 +12,7 @@ import org.springframework.context.annotation.Profile;
 import com.maikon.course.entities.Category;
 import com.maikon.course.entities.Order;
 import com.maikon.course.entities.OrderItem;
+import com.maikon.course.entities.Payment;
 import com.maikon.course.entities.Product;
 import com.maikon.course.entities.User;
 import com.maikon.course.entities.enums.OrderStatus;
@@ -81,6 +82,11 @@ public class TestConfig implements CommandLineRunner{
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 		
 	}
 }
--- src/main/java/com/maikon/course/entities/Order.java
+++ src/main/java/com/maikon/course/entities/Order.java
@@ -6,6 +6,7 @@ import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -13,6 +14,7 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
@@ -35,9 +37,12 @@ public class Order implements Serializable {
 	@ManyToOne // Determina que essa será uma chave estrangeira muitos para um.
 	@JoinColumn(name = "client_id")
 	private User client;
-	
+
 	@OneToMany(mappedBy = "id.order")
 	private Set<OrderItem> items = new HashSet<>();
+	
+	@OneToOne(mappedBy = "order", cascade =CascadeType.ALL)
+	private Payment payment;
 
 	public Order() {
 	}
@@ -83,7 +88,15 @@ public class Order implements Serializable {
 	public void setCliente(User cliente) {
 		this.client = cliente;
 	}
-	
+
+	public Payment getPayment() {
+		return payment;
+	}
+
+	public void setPayment(Payment payment) {
+		this.payment = payment;
+	}
+
 	public Set<OrderItem> getItems() {
 		return items;
 	}
